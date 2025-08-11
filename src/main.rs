60963671f355e30669de37c5fe0d892d25b745d3
nirdesh-dev <nbhattarai.work@gmail.com>
2025-08-11 07:22:42 +0545

    Implemented basic pcap file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use pcap_file::pcap::PcapReader;
+use std::fs::File;
+
+fn main() -> Result<()> {
+    let file_in = File::open("mdf-kospi200.20110216-0.pcap").context("pcap file not found")?;
+    let mut pcap_reader = PcapReader::new(file_in)?;
+
+    // Read the pcap file
+    while let Some(pkt) = pcap_reader.next_packet() {
+        let pkt = pkt.unwrap();
+        println!("{:?}", pkt);
+    }
+
+    Ok(())
 }
